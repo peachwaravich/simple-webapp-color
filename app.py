e25aa59607ec135e258d423256b0c57045510560
Cyber Peach <36033754+peachwaravich@users.noreply.github.com>
2022-08-02 15:38:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ def read_file():
     return render_template('./templates/hello.html', name=socket.gethostname(), contents=contents, color=color_codes[color])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8081")
+    app.run(host="0.0.0.0", port="8080")
